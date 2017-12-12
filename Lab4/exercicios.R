42171fc9a3443ce1dcfc0f1abd34b9a54a6fe0e6
pablosls <logonrm@fiap.com.br>
2017-12-11 22:07:39 -0200

    add

--- Lab4/exercicios.R
+++ Lab4/exercicios.R
@@ -45,7 +45,6 @@ selecionando4
 
 library(tcltk)
 
-#edmar_caldas@yahoo.com.br
 
 
 valor <- tkmessageBox(title = "Arquivo", message = "Olá Mundo", icon="info", type="ok")
@@ -54,5 +53,61 @@ valor
 attach(Banco)
 qqnorm(salário);qqline(salário, col="blue")
 # verificar se a variavel tem tendencia 
+
+
+
+library("rvest")
+pagina <- html("http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=40940&module=M")
+dados <-pagina %>%
+  html_nodes(".dxgvDataRow")%>%
+  html_text()
+dados <-gsub("rnttt","", dados)
+dados <-gsub("[[:punct:]]","",dados)
+data<-substr(dados,1,11)
+risco<-substr(dados,12,100)
+risco<-gsub("[[:punct:]]","",risco)
+tabela <-data.frame(data,risco,stringsAsFactors = FALSE)
+tabela
+
+
+x <- 0.3
+if( x < 0.2)
+{
+  x <- x+ 1
+}else{
+  x <- x-1
+}
+x
+
+
+
+x <- seq(10,100,by=5)
+x
+z <- mean(x)
+z
+
+y <- ifelse(x>=z, "Aprovado", "reprovado")
+y
+
+
+
+x<-2
+fun <- function(x)
+{
+  3*x+1
+}
+fun(x) + 10
+
+show(Apend_A)
+
+
+x <- Apend_A$Idade
+x
+
+Apend_A$idadeMulti <- fun(x)
+Apend_A
+
+
+
 
 
